2ee10d94352f29df9ebd1eeb89e94e2b7d2fa4c3
Umutoni Rita <umurita37@gmail.com>
2025-05-20 14:10:42 +0200

    3rd commit

--- server/controllers/carEntryController.js
+++ server/controllers/carEntryController.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const { getUserFromToken } = ("../middleware/auth.js");
+const getUserFromToken = ("../middleware/auth.js");
 
 const registerCarEntry = async (req, res) => {
   try {
